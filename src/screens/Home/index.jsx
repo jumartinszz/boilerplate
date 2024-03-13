86ff9bf60f61ca11dae7b43b9086f1017a5c2272
Julia Martins <juliamartinss.564@gmail.com>
2024-03-13 08:41:05 -0300

    feat: create profile and category screns

---
+++ src/screens/Home/index.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+import Title from '../../components/Title';
+import React from 'react';
+
+export default function Home() {
+  return (
+    <View style={StyleSheet.container}>
+      <Title title={"Home - Hello today birthday felipe dev"}/>
+    </View>
+  )
+}
